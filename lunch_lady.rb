79dbdbbdf01cdcd02c81e232938b81596ebb0e8f
Karson Hatch <karsonhatch@gmail.com>
2016-02-22 09:36:18 -0700

    bruh, totes added lunch lady

---
+++ lunch_lady.rb
@@ -0,0 +1,27 @@
+def main_menu
+	puts """
+	***Welcome to the cafeteria, what would***
+	   ***you like for your main course?***"""
+	puts "
+	1) Meatloaf ~~~~~~~~ #{meat_price}$5.00
+	2) Mystery Meat ~~~~ #{mystery_price}$3.00
+	3) Slop ~~~~~~~~~~~~ #{slop_price}$1.00
+	"
+
+end
+
+def sides
+	puts """
+	***What sides would you like?***"""
+	puts "
+	1) Carrots ~~~~~~~~~ $1.75
+	2) Mystery Yogut ~~~ $0.50
+	3) Beef Jerky ~~~~~~ $1.00
+	"
+end
+
+	main_menu
+	sides 
+
+# Before you say "Well this isn't even a project, its a piece of shite" just run this in terminal and TELL me 
+# those menues aren't lined up to PERFECTION. 
